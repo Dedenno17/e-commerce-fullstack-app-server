84f2ca7311c78f82c8588a04935cd14b0efb961c
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2023-01-23 08:52:44 +0700

    membuta kerangka routes

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import helmet from 'helmet';
 
+// routes
+import userRoutes from './routes/user.js';
+import authRoutes from './routes/auth.js';
+import cartRoutes from './routes/cart.js';
+import productRoutes from './routes/product.js';
+
 // SETUP
 const app = express();
 dotenv.config();
@@ -16,6 +22,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('common'));
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+
+// ROUTES
+app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/cart', cartRoutes);
+app.use('/api/products', productRoutes);
 
 // connect to mongodb
 mongoose.set('strictQuery', false);
