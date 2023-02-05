3ceb92bfe1cb94305881d8391adcccdbeb30911b
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2023-02-05 14:41:22 +0700

    memperbaiki cartRoutes

--- controllers/cart.js
+++ controllers/cart.js
@@ -17,6 +17,9 @@ export const getCart = async (req, res) => {
   const id = req.params.userId;
   try {
     const userCart = await Cart.findOne({ userId: id });
+    if (!userCart) {
+      res.status(404).json('Not Found');
+    }
     res.status(200).json(userCart);
   } catch (error) {
     res.status(500).json({ message: error.message });
